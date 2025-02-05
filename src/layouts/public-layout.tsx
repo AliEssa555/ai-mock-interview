4939ab76690b759e6d6ebc175eb1953ff8265580
SyedAli777 <125679763+SyedAli777@users.noreply.github.com>
2025-02-05 14:10:17 +0500

    Changes : Home Screen Completed

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/ui/footer"
 import Header from "@/components/ui/header"
+import { Footer } from "@/components/ui/footer"
 import AuthHandler from "@/handlers/auth-handlers"
 import { Outlet } from "react-router-dom"
 
